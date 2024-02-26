ed87b8e3b9154291b1fbfc80995921d6ee638136
Paliienko Artem <66407177+apaliienko@users.noreply.github.com>
2024-02-26 17:54:58 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -11,12 +11,12 @@ if len(sys.argv)>1:
     if len(sys.argv) > 2:
         message = (x.encode() for x in sys.argv[2:])
 
-sockobj = socket(AF_INET, SOCK_STREAM)
-sockobj.connect((serverHost, serverPort))
+socketobj = socket(AF_INET, SOCK_STREAM)
+socketobj.connect((serverHost, serverPort))
 
 for line in message:
-    sockobj.send(line)
-    data = sockobj.recv(1024)
+    socketobj.send(line)
+    data = socketobj.recv(1024)
     print('Client received:', data)
 
-sockobj.close()
+socketobj.close()
